6b192f66ccf821a46790a0ba68db27836a0f5ebe
Myka Kugaya <mykakug129@gmail.com>
2020-11-15 03:37:16 -0800

    Updates

---
+++ precache-manifest.3ec5a29265fb8f30d5b5da14443232fa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24eb182f052dbb5785a18781c488a96e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db9de1e7316c1e3dec08",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "369f7e41f375f007ee3c",
+    "url": "/static/css/main.5ad1c422.chunk.css"
+  },
+  {
+    "revision": "db9de1e7316c1e3dec08",
+    "url": "/static/js/2.20e80ff9.chunk.js"
+  },
+  {
+    "revision": "213bca7ce55773509172dbee394af2a1",
+    "url": "/static/js/2.20e80ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "369f7e41f375f007ee3c",
+    "url": "/static/js/main.5d40feba.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "60180d7b6ae42dee386426ba4d39e190",
+    "url": "/static/media/bluedownarrow.60180d7b.svg"
+  },
+  {
+    "revision": "b14a150ec1559604757abec5d9d75896",
+    "url": "/static/media/github-logo.b14a150e.png"
+  },
+  {
+    "revision": "e054023adcfab496b84501a5f7ed9a5b",
+    "url": "/static/media/jothefish.e054023a.png"
+  },
+  {
+    "revision": "94bb831dc0cba48da2b5c0b736b9add1",
+    "url": "/static/media/jothefishPreview.94bb831d.PNG"
+  },
+  {
+    "revision": "c0d846da2ba340e20b24ab99c414355a",
+    "url": "/static/media/monopoly.c0d846da.jpg"
+  },
+  {
+    "revision": "24bd4feffce53fc0903969dad9189020",
+    "url": "/static/media/monopolyPreview.24bd4fef.jpg"
+  },
+  {
+    "revision": "dd20e516699a3e14a40cff8db468f716",
+    "url": "/static/media/myka.dd20e516.JPG"
+  },
+  {
+    "revision": "853cf5ac7ba3f1f5c3e588f0dba38aeb",
+    "url": "/static/media/resume.853cf5ac.PNG"
+  },
+  {
+    "revision": "cf230ed885704eafde84fe7aa0a6fa93",
+    "url": "/static/media/spacehub.cf230ed8.jpg"
+  },
+  {
+    "revision": "9d2e8c0f7a907eca16d4a844c1f3cf3e",
+    "url": "/static/media/spacehubPreview.9d2e8c0f.jpg"
+  },
+  {
+    "revision": "0f9bcbb15b3b5f084e2581a6140d1207",
+    "url": "/static/media/whitedownarrow.0f9bcbb1.svg"
+  }
+]);
