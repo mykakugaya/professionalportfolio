9c3451b5d5f668922237820d5e39f2364401a5eb
Myka Kugaya <mykakug129@gmail.com>
2020-11-15 03:38:03 -0800

    deploy changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require('path');
 require("dotenv").config();
 
 const app = express();
@@ -14,11 +15,15 @@ app.use(compression());
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 // Add routes, both API and view
 app.use("/api", routes);
 
